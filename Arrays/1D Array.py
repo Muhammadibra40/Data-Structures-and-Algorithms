1ea5ec1c362aff28bd970f80968f26bd984e8157
Muhammadibra40 <116669316+Muhammadibra40@users.noreply.github.com>
2025-07-05 01:23:10 +0300

    sync

--- Arrays/1D Array.py
+++ Arrays/1D Array.py
@@ -1,4 +1,5 @@
 import array
+import numpy as np
 
 # This code snippet demonstrates how to create a 1D array using the array module in Python and print its elements.
 arr_1 = array.array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
@@ -7,3 +8,13 @@ arr_1 = array.array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 for i in arr_1:
     print(i, end=' ')
 
+print("\n\n")
+# Arrays only accepts the same data types but list accepts different data types.
+
+# nums in the array will be treated as chars.
+arr = np.array([1, 2, 3, 4, 5, 'a'])
+list = [1, 2, 3, 4, 'a']
+
+
+print(arr)
+print(list)
---
+++ Lists/listOperations.py
@@ -0,0 +1,121 @@
+# Lists can have different types of elements.
+# Lists can have nested lists
+
+list1 = ["Milk", "coffee", "banana"]
+
+list1[:2] = ["Jam", "oil"]
+
+# list1[3] = "berries" Out of index error and doesn't add this element to the list.
+
+# Insertion of an element in a specific index and the elements after will be shifted O(n) time complexity.
+list1.insert(1, 55)
+print(list1)
+
+# Insertion of an element at the end of the list.
+list1.append(66)
+list1.append([222])
+print(list1)
+
+# Extension of a liat by adding another list to it. O(n) time & space complexity as it iterates through all the elements of the new list and add them.
+list2 = [5, 6, 7, 8]
+list1.extend(list2)
+print(list1)
+
+
+# Deletion methods in Lists.
+# 1. pop()
+# 2. remove()
+# 3. del
+
+# time complexity without using an index = O(1)/ time complexity of a specified index, aslo if it was the first element = O(n)
+# space complexity = O(1)
+deleted_element = list1.pop(1)
+
+# The last element.
+deleted_element = list1.pop()
+
+print(list1)
+
+# time complexity = O(n)
+# space complexity = O(1)
+del list1[:2]
+print(list1)
+
+# Deletion of an element without knowing its index.
+# time complexity = O(n)
+# space complexity = O(1)
+list1.remove(66)
+print(list1)
+
+# --------------------------------------------------------------------------------------------- #
+# Searching in Lists.
+# --------------------------------------------------------------------------------------------- #
+
+# 1. in operator: performs a linear search under the hood.
+# Time complexity = O(1)
+new_list = [1, 2, 3, 4, "Mango"]
+
+print(1 in new_list)
+
+# 2. Linear Search
+
+# --------------------------------------------------------------------------------------------- #
+# Operators/ Functions in Lists.
+# --------------------------------------------------------------------------------------------- #
+# + Operator: concatination of lists.
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+
+print(c)
+
+# * Operator: Repeatation of elements.
+a = a * 2
+print(a)
+
+# a = a * b
+# print(a)
+
+# max()/ min() funtion: returns the max/ min value in a list.
+
+max_val = max(a)
+print(max_val)
+
+# TypeError: '>' not supported between instances of 'int' and 'str'.
+# max only used in a list with the same datatypes.
+# max_val = max(list1)
+# print(max_val)
+
+str_list = ["apple", "cake", "orange"]
+print(max(str_list))
+
+# sum()/ len()
+
+
+# num_list = []
+# while(True):
+#     in_val = input("Enter a number: ")
+#     if in_val == 'done': break
+#     num_list.append(int(in_val))
+# print('Average: ', sum(num_list)/len(num_list))
+
+
+# list() function
+
+str = "spam"
+str_list = list(str)
+print(str_list)
+
+# split() mehtod
+
+str_ex = "spam-spam-spam"
+list_ex = str_ex.split("-")
+print(list_ex)
+
+# join() function
+list_ex = "-".join(list_ex)
+print(list_ex)
+
+# sort() method "in-place" vs. sorted() function "out-of-place"
+
